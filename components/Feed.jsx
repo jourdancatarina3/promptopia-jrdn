ebe85efa29aa9a3386a235ff32de49b147312f28
jourdancatarina <jourdancatarina3@gmail.com>
2023-07-17 11:03:31 +0800

    Add the search feature, Add the clicktag feature, and Add the open profile feature

--- components/Feed.jsx
+++ components/Feed.jsx
@@ -20,10 +20,31 @@ const PromptCardList = ({ data, handleTagClick }) => {
 
 const Feed = () => {
   const [searchText, setSearchText] = useState('');
-  const [posts, setPosts] = useState([])
+  const [posts, setPosts] = useState([]);
+
+  
+  console.log("posts", posts.map((post) => post.prompt));
+
+  const searchFilter = (array) => {
+    return array.filter((post) =>(
+      post.prompt.toLowerCase().includes(searchText.toLowerCase()) 
+      || post.tag.toLowerCase().includes(searchText.toLowerCase()) 
+      || post.creator.username.toLowerCase().includes(searchText.toLowerCase()) 
+    )
+    );
+  };
+
+  const handleTagClick = (tagName) => {
+    setSearchText(tagName);
+  };
+  
+  
+
+  const filtered = searchFilter(posts)
+  console.log("filtered", filtered);
 
   const handleSearchChange = (e) => {
-
+    setSearchText(e.target.value)
   }
 
   useEffect(() => {
@@ -51,8 +72,8 @@ const Feed = () => {
       </form>
 
       <PromptCardList 
-        data={posts}
-        handleTagClick={() => {}}
+        data={filtered}
+        handleTagClick={handleTagClick}
       />
     </section>
   )
--- components/PromptCard.jsx
+++ components/PromptCard.jsx
@@ -12,6 +12,10 @@ const PromptCard = ({ post, handleTagClick, handleEdit, handleDelete }) => {
 
   const [copied, setCopied] = useState("");
 
+  const handleProfileClick = () => {
+    router.push(`/prompt-profile?id=${post.creator._id}`);
+}
+
   const handleCopy = () => {
     setCopied(post.prompt);
     navigator.clipboard.writeText(post.prompt);
@@ -21,7 +25,10 @@ const PromptCard = ({ post, handleTagClick, handleEdit, handleDelete }) => {
   return (
     <div className="prompt_card">
       <div className="flex justify-between items-start gap-5" >
-        <div className="flex-1 flex justify-start items-center gap-3 cursor-pointer">
+        <div 
+          className="flex-1 flex justify-start items-center gap-3 cursor-pointer"
+          onClick={handleProfileClick}
+          >
           <Image
             src={post.creator.image}
             alt="user_image"
@@ -53,6 +60,21 @@ const PromptCard = ({ post, handleTagClick, handleEdit, handleDelete }) => {
       </p>
 
       {session?.user.id === post.creator._id && pathName === '/profile' && (
+        <div className="mt-5 flex-center gap-4 border-t border-gray-100 pt-3">
+          <p 
+          className="font-inter text-sm green_gradient cursor-pointer"
+          onClick={handleEdit}>
+            Edit
+          </p>
+          <p 
+          className="font-inter text-sm orange_gradient cursor-pointer"
+          onClick={handleDelete}>
+            Delete
+          </p>
+        </div>
+      )}
+
+      {pathName === `/prompt-profile` && (
         <div className="mt-5 flex-center gap-4 border-t border-gray-100 pt-3">
           <p 
           className="font-inter text-sm green_gradient cursor-pointer"
